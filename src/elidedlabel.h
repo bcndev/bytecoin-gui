eb3399de10c90dbc1b844d16579159c7807c5b80
Dev <>
2021-02-02 22:47:22 +0100

    fixed undefined reference to vtable for WalletGUI::ElidedLabel

--- src/elidedlabel.h
+++ src/elidedlabel.h
@@ -12,6 +12,7 @@ public:
     using QLabel::QLabel;
     void setElideMode(Qt::TextElideMode elideMode);
     Qt::TextElideMode elideMode() const { return m_elideMode; }
+    virtual ~ElidedLabel() { };
 
 protected:
     void paintEvent(QPaintEvent *e) override;
