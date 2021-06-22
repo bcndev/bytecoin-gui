57b32e30795cbe970a3f5eea29defbb0cb8c028b
Dev <dev@>
2021-06-22 18:05:18 +0200

    Updated version number

--- src/version.h
+++ src/version.h
@@ -5,7 +5,7 @@
 
 namespace WalletGUI {
 
-constexpr char VERSION[] = "0.1.0";
+constexpr char VERSION[] = "0.1.1";
 constexpr char CODENAME[] = "Armor";
 constexpr char VERSION_SUFFIX[] = "stable";
 constexpr char REVISION[] = "20210622";
