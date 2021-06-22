51dff972c3ec54185979f454b3dbd1682d62596a
Dev <dev@>
2021-06-22 13:49:29 +0200

    Updated for V5

--- src/version.h
+++ src/version.h
@@ -5,10 +5,10 @@
 
 namespace WalletGUI {
 
-constexpr char VERSION[] = "0.0.3";
+constexpr char VERSION[] = "0.1.0";
 constexpr char CODENAME[] = "Armor";
 constexpr char VERSION_SUFFIX[] = "stable";
-constexpr char REVISION[] = "20210605";
+constexpr char REVISION[] = "20210622";
 
 // returns <0, if newVersion is worse than currentVersion, returns >0, if newVersion is better, and returns 0, if versions are equal
 int compareVersion(const QString& newVersion, const QString& currentVersion);
